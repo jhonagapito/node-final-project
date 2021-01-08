6a4fdd937f9a7404913444de72049f9273ee6b1c
Jhon Agapito <jhona@magenic.com>
2021-01-08 23:45:22 +0800

    removed mongo_uri

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-//MONGO_URI=mongodb+srv://jhonagapito:C0x3KQ8HcUGTkkGX@project-invoker.gguyd.mongodb.net/demoDb?retryWrites=true&w=majority
+
 async function dbConnect() {
     const connect = await mongoose.connect(process.env.MONGO_URI, {
         useUnifiedTopology: true,
